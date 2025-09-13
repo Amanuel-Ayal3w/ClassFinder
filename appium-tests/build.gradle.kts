8925d26af7eb6433b622ade3425136926975710f
ba5liel <baslielselamu2018@gmail.com>
2025-09-13 18:05:57 +0300

    setting update

--- appium-tests/build.gradle.kts
+++ appium-tests/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-repositories {
-    mavenCentral()
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
